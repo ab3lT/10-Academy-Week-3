9266c8885425aff6328a9aa08c2dc881671c94ae
Abel <se.abel.tadesse@gmail.com>
2024-12-25 22:20:18 +0300

    function documentation

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -4,7 +4,13 @@ import zipfile
 
 
 def extract_zip(zip_file_path: str, extract_to: str) -> None:
-    
+    """
+    Extracts a zip file to the specified directory.
+
+    Args:
+        zip_file_path (str): The path to the zip file.
+        extract_to (str): The directory where the zip contents will be extracted.
+    """
     with zipfile,zipfile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
         
