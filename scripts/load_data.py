7d9e0e68f0bad7e70bbc42eab6e0e53371c7fa75
Abel <se.abel.tadesse@gmail.com>
2024-12-25 22:45:05 +0300

    nested zip

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,5 +1,6 @@
 import os
 import zipfile
+import io
 
 
 
@@ -11,6 +12,19 @@ def extract_zip(zip_file_path: str, extract_to: str) -> None:
         zip_file_path (str): The path to the zip file.
         extract_to (str): The directory where the zip contents will be extracted.
     """
-    with zipfile,zipfile(zip_file_path, 'r') as zip_ref:
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
+       
+def extract_nested_zip(outer_zip_path: str, extract_to: str) -> None:
+    with zipfile.ZipFile(outer_zip_path, 'r') as outer_zip:
+        for file_info in outer_zip.infolist():
+            if file_info.filename.endswith(".zip"):
+                
+                with outer_zip.open(file_info.filename) as nested_zip_file:
+                    nested_zip_data = io.BytesIO(nested_zip_file.read())
+                    with zipfile.ZipFile(nested_zip_data, 'r') as nested_zip:
+                        nested_zip.extractall(extract_to)
+            else:
+                outer_zip.extract(file_info, extract_to)
         
+    
